1777ad67f671b8ac5a9b43d22bb97aa12ed18fb6
Ron Shalevich <ronshalevich@Rons-MacBook-Air.local>
2017-07-19 21:38:14 -0700

    final product

--- js/script.js
+++ js/script.js
@@ -17,9 +17,11 @@ function loadData() {
 
     $greeting.text('So, you want to live at ' + address + '?');
 
+    // google street view API
     var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append ('<img class="bgimg" src="' + streetviewUrl +'">');
 
+    // NY Times API
     var nytimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=c03ba67ed60d436cb2575afd3629c04a'
     $.getJSON(nytimesURL, function(data){
       $nytHeaderElem.text('New York Times Articles About ' + cityStr);
@@ -32,12 +34,13 @@ function loadData() {
       $nytHeaderElem.text('New York Times Articles Could Not Be Loaded')
     })
 
+    // Wikipedia API
     var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + ' &format=json&callback=wikiCallback';
 
     var wikiRequestTimeout = setTimeout(function(){
       $wikiElem.text("failed to get wikipedia resources");
     }, 8000);
-    
+
     $.ajax({
       url: wikiUrl,
       dataType: "jsonp",
