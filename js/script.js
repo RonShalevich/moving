b9775ff04190c1ce0e828f1ff4e2083b077ab693
Ron Shalevich <ronshalevich@Rons-MacBook-Air.local>
2017-07-19 21:44:46 -0700

    hide key

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ function loadData() {
     $body.append ('<img class="bgimg" src="' + streetviewUrl +'">');
 
     // NY Times API
-    var nytimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=c03ba67ed60d436cb2575afd3629c04a'
+    var nytimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=xxxxxxxxxxxxxxxxxxxxxx'
     $.getJSON(nytimesURL, function(data){
       $nytHeaderElem.text('New York Times Articles About ' + cityStr);
       articles = data.response.docs;
